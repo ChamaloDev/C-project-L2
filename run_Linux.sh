5705930b04ce562cb44926c6b6af0c5685d2602a
Nhessi <91568598+ChamaloDev@users.noreply.github.com>
2025-01-31 15:04:11 +0100

    Update run_Linux.sh

--- run_Linux.sh
+++ run_Linux.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 # Set variables
+CC=gcc
+CFLAGS="-I./include/Linux/SDL2 -I./include/Linux/SDL2_ttf"
+LDFLAGS="-L./lib/SDL2 -L./lib/SDL2_ttf -lSDL2main -lSDL2_ttf -lSDL2"
+
+SRC="src/*.c"
+OBJ="main.o"
 EXEC="game"
 
 # Create bin directory if it doesn't exist
 mkdir -p bin
 
-# Compiling code
-gcc -std=c17 src/*.c -Wall -Wextra -o bin/$EXEC $(sdl2-config --cflags --libs) -lSDL2_ttf
+# Compile source files into object files
+$CC -std=c17 -Wall -Wextra $CFLAGS -c $SRC -o bin/$OBJ
+# Link object files into the executable
+$CC -o bin/$EXEC bin/$OBJ $LDFLAGS
+# Remove the object file
+rm bin/$OBJ
 
 # Change to the bin directory
 cd bin
